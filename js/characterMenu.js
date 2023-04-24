a3344385c66e884362e2aeda83a9e6feeef23eba
Shaked-BA <shaked.ba3@gmail.com>
2023-04-24 16:02:14 +0300

    added health bar

--- js/characterMenu.js
+++ js/characterMenu.js
@@ -27,4 +27,5 @@ var onCharacterClick = function(event) {
     setVisibility('playing-background', 'inline-block');
     setVisibility('gameCanvas', 'block');
     setVisibility('game-score-container', 'flex');
+    setVisibility('health-bar', 'inline-block')
 };
